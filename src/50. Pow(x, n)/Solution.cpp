f40ddfef7c9d30875f59b7a76fe0c5e19d69591b
AssoDePicche <samuelprado730@gmail.com>
2023-07-24 19:05:44 -0300

    feat: add 50. pow(x, n)

---
+++ src/50. Pow(x, n)/Solution.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include "./Solution.hpp"
+
+double Solution::myPow(double base, int exponent) {
+  if (exponent == 0) return 1.0;
+
+  if (exponent < 0) {
+    base = (1.0 / base);
+
+    exponent = abs(exponent);
+  }
+
+  double power = 1.0;
+
+  while (exponent > 0) {
+    if (exponent % 2 == 1) {
+      power *= base;
+    }
+
+    base *= base;
+
+    exponent /= 2;
+  }
+
+  return power;
+}
---
+++ src/50. Pow(x, n)/Solution.hpp
@@ -0,0 +1,7 @@
+#ifndef SOLUTION_H_
+#define SOLUTION_H_
+class Solution {
+public:
+    double myPow(double base, int exponent);
+};
+#endif
