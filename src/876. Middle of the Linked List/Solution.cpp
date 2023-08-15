db915061fda936f06e96c9226d9a846be72a95e9
AssoDePicche <samuelprado730@gmail.com>
2023-08-15 10:44:34 -0300

    perf: reduce iterations

--- src/876. Middle of the Linked List/Solution.cpp
+++ src/876. Middle of the Linked List/Solution.cpp
@@ -2,33 +2,14 @@
 
 ListNode *Solution::middleNode(ListNode *head)
 {
-  int listSize = 0;
+  ListNode *slowPointer = head;
 
-  int middle = 0;
-
-  ListNode *begin = head;
-
-  ListNode *nextNode = nullptr;
-
-  while (begin->next != nullptr)
+  while (head != nullptr && head->next != nullptr)
   {
-    nextNode = begin->next;
+    slowPointer = slowPointer->next;
 
-    begin = nextNode;
-
-    ++listSize;
+    head = head->next->next;
   }
 
-  middle = (listSize % 2 == 0) ? (listSize / 2) : (listSize / 2) + 1;
-
-  while (middle != 0)
-  {
-    nextNode = head->next;
-
-    head = nextNode;
-
-    --middle;
-  }
-
-  return head;
+  return slowPointer;
 }
